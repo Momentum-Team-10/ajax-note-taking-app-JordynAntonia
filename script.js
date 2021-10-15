b57253f8f7b763210bdb641f9921e0cac54ebc49
Jordyn Price <88625349+JordynAntonia@users.noreply.github.com>
2021-10-14 23:26:26 -0400

    commit

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log(`Hello World!`)
+
+const url = `http://localhost:3000/notes`
+const note = document.getElementById(`note`)
+
+function notes() {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        for (let item of data){
+            renderNoteItem(item)
+        }
+    })
+}
+function renderNoteItem(noteOnj) {
+    const li = documtent.createElement(`li`)
+    li.id = noteObj.id
+    li.classlist.add(
+        
+    )
+}
+    fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
